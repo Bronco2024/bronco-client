547d87c357d99a9a77f87363041926483f41db96
Haytham Taweel <haythamtaweel95@gmail.com>
2025-08-22 18:41:12 +0300

    split databases added envs

--- src/firebase-test.js
+++ src/firebase-test.js
@@ -12,13 +12,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA6_yXQbkbcgHQCLnhKSKmtaBltCrxRr8I",
-  authDomain: "bronco-testing.firebaseapp.com",
-  projectId: "bronco-testing",
-  storageBucket: "bronco-testing.firebasestorage.app",
-  messagingSenderId: "92996437306",
-  appId: "1:92996437306:web:383a44092081cc1f6bbc03",
-  measurementId: "G-15XNYDBNNV"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
--- src/firebase.js
+++ src/firebase.js
@@ -12,13 +12,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBGnw-nPU2Co-Mb17bmmYwgJly05czunug",
-    authDomain: "bronco-65aaf.firebaseapp.com",
-    projectId: "bronco-65aaf",
-    storageBucket: "bronco-65aaf.appspot.com",
-    messagingSenderId: "317603302387",
-    appId: "1:317603302387:web:2525316aaf18e677936086",
-    measurementId: "G-SNSKQ3MYND"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
